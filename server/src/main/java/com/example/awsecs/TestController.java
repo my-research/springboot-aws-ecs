f040d426c0e77b0e7d24c4b6bc63e265ddfa574f
dhslrl321 <dhslrl321@gmail.com>
2021-11-15 03:09:03 +0900

    commit

---
+++ server/src/main/java/com/example/awsecs/TestController.java
@@ -0,0 +1,18 @@
+package com.example.awsecs;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity test() {
+
+        return ResponseEntity.ok().build();
+    }
+
+}
