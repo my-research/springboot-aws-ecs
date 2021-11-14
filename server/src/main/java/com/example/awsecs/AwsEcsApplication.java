b5d1f9e75bfeaba717330ad3d5d8d9b2d05675d9
dhslrl321 <dhslrl321@gmail.com>
2021-11-15 02:04:43 +0900

    commit

---
+++ server/src/main/java/com/example/awsecs/AwsEcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsEcsApplication.class, args);
+    }
+
+}
