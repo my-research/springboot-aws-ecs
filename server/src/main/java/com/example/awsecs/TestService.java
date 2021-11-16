4d399bc7a42fb415428bf6cc1b8f4d40bf97ec18
dhslrl321 <dhslrl321@gmail.com>
2021-11-16 20:05:29 +0900

    commit

---
+++ server/src/main/java/com/example/awsecs/TestService.java
@@ -0,0 +1,12 @@
+package com.example.awsecs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Service;
+import
+
+@RequiredArgsConstructor
+@Service
+public class TestService {
+
+}
